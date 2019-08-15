92835f0368c88cb6f2b9bc4708025ab96ffaecfc
sobolevn <mail@sobolevn.me>
2019-08-15 23:13:34 +0300

    Fixes ci

--- tests/test_io/test_io_equality.py
+++ tests/test_io/test_io_equality.py
@@ -8,12 +8,12 @@ from returns.io import IO
 def test_equality():
     """Ensures that containers can be compared."""
     assert IO(1) == IO(1)
-    assert str(IO(1)) == '<IO: 1>'
-    assert hash(IO(1))
+    assert str(IO(2)) == '<IO: 2>'
+    assert hash(IO((1, 2, 3)))
 
 
 def test_nonequality():
     """Ensures that containers are not compared to regular values."""
     assert IO(1) != 1
-    assert IO(1) is not IO(1)
-    assert IO(1) != IO(2)
+    assert IO(2) is not IO(2)
+    assert IO('a') != IO('b')
