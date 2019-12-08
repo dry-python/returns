5beb8678846c2aa27b4167d04fb4568d14c25aaa
sobolevn <mail@sobolevn.me>
2019-12-08 12:50:36 +0300

    Fixes ci

--- tests/test_io/test_io_pickle.py
+++ tests/test_io/test_io_pickle.py
@@ -5,11 +5,11 @@ from returns.io import IO
 
 def test_io_pickle():
     """Tests how pickle protocol works for containers."""
-    assert IO(1).__getstate__() == 1
+    assert IO(1).__getstate__() == 1  # noqa: WPS609
 
 
 def test_io_pickle_restore():
     """Ensures that object can be restored."""
     container = IO(2)
-    container.__setstate__(1)
+    container.__setstate__(1)  # noqa: WPS609
     assert container == IO(1)
