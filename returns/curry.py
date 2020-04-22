a9217cf50a52946c117a7638332ade2feb78ef26
Gram <master_fess@mail.ru>
2020-04-22 12:56:37 +0200

    init

--- returns/curry.py
+++ returns/curry.py
@@ -1,4 +1,5 @@
 from functools import partial as _partial
+from inspect import getcallargs
 from typing import Any, Callable, TypeVar
 
 _ReturnType = TypeVar('_ReturnType')
@@ -31,3 +32,56 @@ def partial(
 
     """
     return _partial(func, *args, **kwargs)
+
+
+class EagerCurry:
+    """Currying that calls the wrapped function when enough arguments are passed.
+
+    Currying is a conception from functional languages that does partial
+    applying. That means that if we pass one argument in a function that
+    get 2 or more arguments, we'll get a new function that remembers all
+    previously passed arguments. Then we can pass remaining arguments, and
+    the function will be executed.
+
+    ``partial`` sunction does a similar thing but it does partial application
+    exactly once. ``eager_curry`` is a bit smarter and will do parial
+    application until enough arguments passed.
+
+    The reason why we have 2 implementations of currying is that Python
+    is a bit different from classic functional languages. Python has
+    unpacking and default arguments, hence sometimes we can't say if
+    the user will pass more arguments or the function already must be called.
+    ``eager_curry`` calls the function immediately when enough arguments passed
+    while ``lazy_curry`` wait until the function is explicitly called
+    without arguments.
+    """
+    def __init__(self, func: Callable):
+        self._func = func
+
+    def _enough(self, *args, **kwargs) -> bool:
+        """Returns True if passed arguments are enough to call the function.
+        """
+        try:
+            getcallargs(self._func, *args, **kwargs)
+        except TypeError:
+            return False
+        return True
+
+    def __call__(self, *args, **kwargs):
+        if not self._enough(*args, **kwargs):
+            return partial(self, *args, **kwargs)
+        return self._func(*args, **kwargs)
+
+
+class LazyCurry:
+    def __init__(self, func: Callable):
+        self._func = func
+
+    def __call__(self, *args, **kwargs):
+        if args or kwargs:
+            return partial(self, *args, **kwargs)
+        return self._func(*args, **kwargs)
+
+
+eager_curry = EagerCurry
+lazy_curry = LazyCurry
