ad2b44ec9972c9a9ca81fd46f18811f3a2dc1432
Gram <master_fess@mail.ru>
2020-04-23 15:01:18 +0200

    test more

--- tests/test_curry/test_lazy_curry.py
+++ tests/test_curry/test_lazy_curry.py
@@ -42,6 +42,7 @@ def test_one_arg():
         return a
 
     assert type(func(1)) is partial
+
     # no TypeErrors until the explicit call
     assert type(func(1, 2)) is partial
     assert type(func(1)(2)) is partial
@@ -53,3 +54,83 @@ def test_one_arg():
         func(1, 2)()
     with pytest.raises(TypeError):
         func(1)(2)()
+
+    # rewrite
+    assert func(a=1)(a=2)() == 2
+
+
+def test_two_args():
+    @lazy_curry
+    def func(a, b):
+        return (a, b)
+
+    assert func(1)(2)() == (1, 2)
+    assert func(1, 2)() == (1, 2)
+
+    assert func(1, b=2)() == (1, 2)
+    assert func(a=1, b=2)() == (1, 2)
+    assert func(b=1, a=2)() == (2, 1)
+
+    assert func(b=1)(a=2)() == (2, 1)
+    assert func(a=1)(b=2)() == (1, 2)
+    assert func(1)(b=2)() == (1, 2)
+    assert func(b=1)(2)() == (2, 1)
+
+    with pytest.raises(TypeError):
+        func(1, 2, 3)()
+    with pytest.raises(TypeError):
+        func(1, c=2)()
+    with pytest.raises(TypeError):
+        func(1)(c=2)()
+    with pytest.raises(TypeError):
+        func(1)(2)(3)()
+
+
+def test_star_args():
+    @lazy_curry
+    def func(*args):
+        return args
+
+    assert func() == ()
+    assert func(1)() == (1, )
+    assert func(1, 2)() == (1, 2)
+    assert func(1, 2, 3)() == (1, 2, 3)
+
+    assert func(1)(2)() == (1, 2)
+    assert func(1)(2)(3)() == (1, 2, 3)
+    assert func(1)(2, 3)() == (1, 2, 3)
+    assert func(1, 2)(3)() == (1, 2, 3)
+
+    with pytest.raises(TypeError):
+        func(a=1)()
+    with pytest.raises(TypeError):
+        func(1)(b=2)()
+
+
+def test_star_kwargs():
+    @lazy_curry
+    def func(**kwargs):
+        return sorted(kwargs.items())
+
+    assert func() == []
+    assert func(a=1)() == [('a', 1)]
+    assert func(a=1, b=2)() == [('a', 1), ('b', 2)]
+    assert func(a=1)(b=2)() == [('a', 1), ('b', 2)]
+    assert func(a=1, b=2)(b=3)() == [('a', 1), ('b', 3)]
+    assert func(b=2)(a=1)() == [('a', 1), ('b', 2)]
+
+
+def test_arg_star_kwargs():
+    @lazy_curry
+    def func(a, **kwargs):
+        return [('a', a)] + sorted(kwargs.items())
+
+    assert func(1)() == [('a', 1)]
+    assert func(a=1)() == [('a', 1)]
+    assert func(1, b=2)() == [('a', 1), ('b', 2)]
+    assert func(1)(b=2)() == [('a', 1), ('b', 2)]
+    assert func(a=1)(b=2)() == [('a', 1), ('b', 2)]
+    assert func(c=3, a=1)(b=2)() == [('a', 1), ('b', 2), ('c', 3)]
+
+    with pytest.raises(TypeError):
+        func()
