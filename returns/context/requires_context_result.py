90aa526ba84a1afd2e9585a1b26f3a244bbce184
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-25 16:16:13 -0300

    Changes type of "empty" attribute in "RequiresContextResult" class from "ClassVar[Any]" to "ClassVar[NoDeps]"

--- returns/context/requires_context_result.py
+++ returns/context/requires_context_result.py
@@ -10,6 +10,7 @@ from typing import (
 
 from typing_extensions import final
 
+from returns.context import NoDeps
 from returns.primitives.container import BaseContainer
 from returns.primitives.types import Immutable
 from returns.result import Failure, Result, Success
@@ -100,7 +101,7 @@ class RequiresContextResult(
     ]
 
     #: A convinient placeholder to call methods created by `.from_value()`.
-    empty: ClassVar[Any] = object()
+    empty: ClassVar[NoDeps] = object()
 
     def __init__(
         self,
