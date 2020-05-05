e9b683f20753d1764b3a751a42964501f5c07562
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-05-05 00:30:23 -0300

    Creates "is_result" function

--- returns/instances.py
+++ returns/instances.py
@@ -1,6 +1,13 @@
-from returns.context import RequiresContextIOResult
+from returns.context import (
+    ContextIOResult,
+    ContextResult,
+    RequiresContextIOResult,
+    RequiresContextResult,
+)
+from returns.future import FutureResult
 from returns.io import IO, IOResult
 from returns.primitives.container import BaseContainer
+from returns.result import Result
 
 
 def is_io(container: BaseContainer) -> bool:
@@ -17,3 +24,28 @@ def is_io(container: BaseContainer) -> bool:
 
     """
     return isinstance(container, (IO, IOResult, RequiresContextIOResult))
+
+
+def is_result(container: BaseContainer) -> bool:
+    """
+    Verifies if a container is `` Result`` type.
+
+    .. code:: python
+
+      >>> from returns.maybe import Maybe
+      >>> from returns.result import Success
+
+      >>> assert is_result(Success(1.0)) is True
+      >>> assert is_result(Maybe.from_value(1.0)) is False
+
+    """
+    result_containers = (
+        Result,
+        FutureResult,
+        ContextResult,
+        ContextIOResult,
+        IOResult,
+        RequiresContextResult,
+        RequiresContextIOResult,
+    )
+    return isinstance(container, result_containers)
