de12174a5dff83c53ff616e7735d2ffec22487fa
sobolevn <mail@sobolevn.me>
2020-05-22 10:55:26 +0300

    Fixes CI

--- returns/pointfree.py
+++ returns/pointfree.py
@@ -1,8 +1,8 @@
 from returns._generated.pointfree.apply import _apply as apply
 from returns._generated.pointfree.apply import _apply_by as apply_by
 from returns._generated.pointfree.bind import _bind as bind
+from returns._generated.pointfree.bind_io import _bind_io as bind_io
 from returns._generated.pointfree.bind_result import _bind_result as bind_result
 from returns._generated.pointfree.map import _map as map_
 from returns._generated.pointfree.rescue import _rescue as rescue
 from returns._generated.pointfree.unify import _unify as unify
-from returns._generated.pointfree.bind_io import _bind_io as bind_io
