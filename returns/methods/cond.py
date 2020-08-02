00307ff1c7c5b72d424e2369cb9a13e47305d860
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-08-02 14:50:54 -0300

    Creates `cond` method

---
+++ returns/methods/cond.py
@@ -0,0 +1,45 @@
+from typing import Type, TypeVar
+
+from returns.interfaces.specific.result import ResultLikeN
+from returns.primitives.hkt import KindN, kinded
+
+_ValueType = TypeVar('_ValueType')
+_ErrorType = TypeVar('_ErrorType')
+_ThirdType = TypeVar('_ThirdType')
+
+_ResultKind = TypeVar('_ResultKind', bound=ResultLikeN)
+
+
+def internal_cond(
+    container_type: Type[_ResultKind],
+    is_success: bool,
+    success_value: _ValueType,
+    error_value: _ErrorType,
+) -> KindN[_ResultKind, _ValueType, _ErrorType, _ThirdType]:
+    """
+    Help us to reduce the boilerplate when choosing paths with ``ResultLikeN``.
+
+    .. code:: python
+
+      >>> from returns.methods import cond
+      >>> from returns.result import Failure, Result, Success
+
+      >>> def is_numeric(string: str) -> Result[str, str]:
+      ...     return cond(
+      ...         Result,
+      ...         string.isnumeric(),
+      ...         'It is a number',
+      ...         'It is not a number',
+      ...     )
+
+      >>> assert is_numeric('42') == Success('It is a number')
+      >>> assert is_numeric('non numeric') == Failure('It is not a number')
+
+    """
+    if is_success:
+        return container_type.from_value(success_value)
+    return container_type.from_failure(error_value)
+
+
+#: Kinded version of :func:`~internal_cond`, use it to infer real return type.
+cond = kinded(internal_cond)
