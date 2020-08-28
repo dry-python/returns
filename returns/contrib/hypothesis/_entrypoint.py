9c3558e2cee54bbf7174bc0f653e1bd5fa4b74f6
sobolevn <mail@sobolevn.me>
2020-08-28 12:00:19 +0300

    Checking coverage

--- returns/contrib/hypothesis/_entrypoint.py
+++ returns/contrib/hypothesis/_entrypoint.py
@@ -1,6 +1,3 @@
-from returns.result import Result
-
-
 def hypothesis_setup_hook():
     """
     Used to register all our types as hypothesis strategies.
@@ -10,10 +7,3 @@ def hypothesis_setup_hook():
     But, beware that we only register concrete types here,
     interfaces won't be registered!
     """
-    from hypothesis import strategies as st
-
-    # TODO: add all other container defs
-    st.register_type_strategy(Result, st.one_of(
-        st.builds(Result.from_value),
-        st.builds(Result.from_failure),
-    ))
