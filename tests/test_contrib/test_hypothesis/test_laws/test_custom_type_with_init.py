47c82e220bec637a071ace41fdf646b8b55e92be
sobolevn <mail@sobolevn.me>
2020-09-09 17:30:54 +0300

    Closes #588

---
+++ tests/test_contrib/test_hypothesis/test_laws/test_custom_type_with_init.py
@@ -0,0 +1,29 @@
+from typing import Callable, TypeVar
+
+from returns.contrib.hypothesis.laws import check_all_laws
+from returns.interfaces import mappable
+from returns.primitives.container import BaseContainer
+from returns.primitives.hkt import SupportsKind1
+
+_ValueType = TypeVar('_ValueType')
+_NewValueType = TypeVar('_NewValueType')
+
+
+class _Wrapper(
+    BaseContainer,
+    SupportsKind1['_Wrapper', _ValueType],
+    mappable.Mappable1[_ValueType],
+):
+    _inner_value: _ValueType
+
+    def __init__(self, inner_value: _ValueType) -> None:
+        super().__init__(inner_value)
+
+    def map(  # noqa: WPS125
+        self,
+        function: Callable[[_ValueType], _NewValueType],
+    ) -> '_Wrapper[_NewValueType]':
+        return _Wrapper(function(self._inner_value))
+
+
+check_all_laws(_Wrapper, use_init=True)
