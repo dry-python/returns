9eaa2f8eb23fbfbf99b604a880922330f4e32aa6
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-10-02 22:49:38 -0300

    Creates a hypothesis test with a wrong class implementation that should fail

---
+++ tests/test_contrib/test_hypothesis/test_laws/test_wrong_custom_type_with_init.py
@@ -0,0 +1,35 @@
+from typing import Callable, TypeVar
+
+import pytest
+
+from returns.contrib.hypothesis.laws import check_all_laws
+from returns.interfaces import mappable
+from returns.primitives.container import BaseContainer
+from returns.primitives.hkt import SupportsKind1
+
+pytestmark = pytest.mark.xfail
+
+_ValueType = TypeVar('_ValueType')
+_NewValueType = TypeVar('_NewValueType')
+
+
+class _Wrapper(
+    BaseContainer,
+    SupportsKind1['_Wrapper', _ValueType],
+    mappable.Mappable1[_ValueType],
+):
+    _inner_value: _ValueType
+
+    def __init__(self, inner_value: _ValueType) -> None:
+        super().__init__(inner_value)
+
+    def map(  # noqa: WPS125
+        self,
+        function: Callable[[_ValueType], _NewValueType],
+    ) -> '_Wrapper[_NewValueType]':
+        return _Wrapper(
+            'wrong-{0}'.format(function(self._inner_value)),  # type: ignore
+        )
+
+
+check_all_laws(_Wrapper, use_init=True)
