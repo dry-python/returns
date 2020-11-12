bcac5361185d8543f2a01d54c60ef2e6b684bee3
Nikita Sobolev <mail@sobolevn.me>
2020-11-12 17:04:41 +0300

    Update cond.py

--- returns/pointfree/cond.py
+++ returns/pointfree/cond.py
@@ -3,7 +3,7 @@ from typing import Callable, Optional, Type, TypeVar, Union, overload
 from returns.context import NoDeps
 from returns.interfaces.failable import DiverseFailableN, SingleFailableN
 from returns.methods.cond import internal_cond
-from returns.primitives.hkt import Kinded, KindN, kinded
+from returns.primitives.hkt import Kinded, KindN
 
 _ValueType = TypeVar('_ValueType')
 _ErrorType = TypeVar('_ErrorType')
