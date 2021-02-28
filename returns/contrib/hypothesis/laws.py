5eed86b4e662f0564598629264ef0295dc575261
Nikita Sobolev <mail@sobolevn.me>
2021-02-28 15:28:21 +0300

    Update laws.py

--- returns/contrib/hypothesis/laws.py
+++ returns/contrib/hypothesis/laws.py
@@ -57,7 +57,7 @@ def check_all_laws(
     Note:
         Cannot be used inside doctests because of the magic we use inside.
 
-    See also: 
+    See also:
         - https://sobolevn.me/2021/02/make-tests-a-part-of-your-app
         - https://mmhaskell.com/blog/2017/3/13/obey-the-type-laws
 
