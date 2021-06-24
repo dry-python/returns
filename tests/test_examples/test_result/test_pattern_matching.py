c81e1a73a64a6596eefa8ae0e942b9a1b0197ebd
Pablo Aguilar <pablo.aguilar@outlook.com.br>
2021-06-24 02:25:52 -0300

    Creates Pattern Matching example for `Result` container in `test_examples` directory

---
+++ tests/test_examples/test_result/test_pattern_matching.py
@@ -0,0 +1,28 @@
+from returns.result import Failure, Success, safe
+
+
+@safe
+def div(first_number: int, second_number: int) -> int:
+    return first_number // second_number
+
+
+match div(1, 0):
+    # Matches if the result stored inside `Success` is `10`
+    case Success(10):
+        print('Result is "10"')
+
+    # Same as above but using match-by-name
+    case Success(inner_value=20):
+        print('Result is "20"')
+
+    # Matches any `Success` instance and binds its value to the `value` variable
+    case Success(value):
+        print('Result is "{0}"'.format(value))
+
+    # Matches if the result stored inside `Failure` is `ZeroDivisionError`
+    case Failure(ZeroDivisionError):
+        print('"ZeroDivisionError" was raised')
+
+    # Matches any `Failure` instance
+    case Failure(_):
+        print('The division was a failure')
