73104c25174cd7ec43e8b096bba477f6a29c10dd
Nikita Sobolev <mail@sobolevn.me>
2021-07-08 12:57:40 +0300

    Simplifies `conftest.py`

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,25 +11,18 @@ from typing_extensions import Final
 pytest_plugins = ['hypothesis.extra.pytestplugin']
 
 PYTHON_VERSION: Final = (sys.version_info.major, sys.version_info.minor)
-PATHS_TO_IGNORE: Final = MappingProxyType({
-    (3, 7): frozenset((
-        Path('tests/test_examples/test_result/test_result_pattern_matching.py'),
-        Path('tests/test_examples/test_maybe/test_maybe_pattern_matching.py'),
-        Path('tests/test_examples/test_io/test_ioresult_container/test_ioresult_pattern_matching.py'),  # noqa: E501
-    )),
-    (3, 8): frozenset((
-        Path('tests/test_examples/test_result/test_result_pattern_matching.py'),
-        Path('tests/test_examples/test_maybe/test_maybe_pattern_matching.py'),
-        Path('tests/test_examples/test_io/test_ioresult_container/test_ioresult_pattern_matching.py'),  # noqa: E501
-    )),
-    (3, 9): frozenset((
+ENABLE_SINCE: Final = MappingProxyType({
+    (3, 10): frozenset((
         Path('tests/test_examples/test_result/test_result_pattern_matching.py'),
         Path('tests/test_examples/test_maybe/test_maybe_pattern_matching.py'),
         Path('tests/test_examples/test_io/test_ioresult_container/test_ioresult_pattern_matching.py'),  # noqa: E501
     )),
 })
 PATHS_TO_IGNORE_NOW: Final = frozenset(
-    path.absolute() for path in PATHS_TO_IGNORE.get(PYTHON_VERSION, frozenset())
+    path.absolute()
+    for since_python, to_ignore in ENABLE_SINCE.items()
+    for path in to_ignore
+    if PYTHON_VERSION < since_python
 )
 
 
