1e7eeee2816dd8525e8596a83dc700648410025d
Nikita Sobolev <mail@sobolevn.me>
2021-08-19 15:54:58 +0300

    Fixes xfail (#1029)
    * Fixes xfail
    
    * Typo
    
    * Typo

--- tests/test_contrib/test_hypothesis/test_laws/test_unsatisfiable_type.py
+++ tests/test_contrib/test_hypothesis/test_laws/test_unsatisfiable_type.py
@@ -1,35 +1,32 @@
 from typing import Callable, TypeVar
 
 import pytest
+from hypothesis.errors import ResolutionFailed
 
 from returns.contrib.hypothesis.laws import check_all_laws
 from returns.interfaces import mappable
 from returns.primitives.container import BaseContainer
 from returns.primitives.hkt import SupportsKind1
 
-pytestmark = pytest.mark.xfail(raises=AssertionError)
+pytestmark = pytest.mark.xfail(raises=ResolutionFailed)
 
 _ValueType = TypeVar('_ValueType')
 _NewValueType = TypeVar('_NewValueType')
 
 
-class _Wrapper(
+class _WithInitNoFlag(
     BaseContainer,
-    SupportsKind1['_Wrapper', _ValueType],
+    SupportsKind1['_WithInitNoFlag', _ValueType],
     mappable.Mappable1[_ValueType],
 ):
-    _inner_value: _ValueType
-
-    def __init__(self, inner_value: _ValueType) -> None:
-        super().__init__(inner_value)
+    """Does not have any ways to be constructed."""
 
     def map(
         self,
         function: Callable[[_ValueType], _NewValueType],
-    ) -> '_Wrapper[_NewValueType]':
-        return _Wrapper(
-            'wrong-{0}'.format(function(self._inner_value)),  # type: ignore
-        )
+    ) -> '_WithInitNoFlag[_NewValueType]':
+        """We need `map` to have `laws`, should not be called."""
+        raise NotImplementedError
 
 
-check_all_laws(_Wrapper, use_init=True)
+check_all_laws(_WithInitNoFlag)
