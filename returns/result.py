163ffaf69c2a01408574993a09aad736bed9ae89
q0w <43147888+q0w@users.noreply.github.com>
2021-12-15 00:57:43 +0300

    Issue-1174 (#1175)
    * Bump wemake-python-styleguide from 0.15.3 to 0.16.0
    
    Bumps [wemake-python-styleguide](https://github.com/wemake-services/wemake-python-styleguide) from 0.15.3 to 0.16.0.
    - [Release notes](https://github.com/wemake-services/wemake-python-styleguide/releases)
    - [Changelog](https://github.com/wemake-services/wemake-python-styleguide/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/wemake-services/wemake-python-styleguide/compare/0.15.3...0.16.0)
    
    ---
    updated-dependencies:
    - dependency-name: wemake-python-styleguide
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Fix lint errors
    
    * Update returns/result.py
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Nikita Sobolev <mail@sobolevn.me>

--- returns/result.py
+++ returns/result.py
@@ -372,7 +372,6 @@ class Failure(Result[Any, _ErrorType]):  # noqa: WPS338
 
     def _get_trace(self) -> Optional[List[FrameInfo]]:
         """Method that will be monkey patched when trace is active."""
-        return None  # noqa: WPS324
 
 
 @final
