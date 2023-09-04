1fba37a7207e43d155436e105275a0c3715cc107
Nikita Sobolev <mail@sobolevn.me>
2023-09-04 18:52:41 +0300

    Typo

--- returns/interfaces/swappable.py
+++ returns/interfaces/swappable.py
@@ -30,7 +30,7 @@ class _LawSpec(LawSpecDef):
         container: 'SwappableN[_FirstType, _SecondType, _ThirdType]',
     ) -> None:
         """
-        Swaping container twice.
+        Swapping container twice.
 
         It ensure that we get the initial value back.
         In other words, swapping twice does nothing.
