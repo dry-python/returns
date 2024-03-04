571954b0144aa8aeb33c07e50a80fc5b899056d3
sobolevn <mail@sobolevn.me>
2024-03-04 12:57:31 +0300

    Fix mypy

--- returns/contrib/hypothesis/laws.py
+++ returns/contrib/hypothesis/laws.py
@@ -176,7 +176,7 @@ def pure_functions() -> Iterator[None]:
 
 def _get_callable_type() -> Any:
     # Helper to accommodate changes in `hypothesis@6.79.0`
-    if Callable in types._global_type_lookup:
+    if Callable in types._global_type_lookup:  # type: ignore
         return Callable
     elif Callable.__origin__ in types._global_type_lookup:  # type: ignore
         return Callable.__origin__  # type: ignore
