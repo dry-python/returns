558cc67d3651c11000a88207d8a724927ec095d8
sobolevn <mail@sobolevn.me>
2024-03-04 12:12:43 +0300

    Update mypy config

--- tests/test_context/test_requires_context/test_context_equality.py
+++ tests/test_context/test_requires_context/test_context_equality.py
@@ -7,12 +7,12 @@ def _same_function(some_arg: int) -> Callable[[float], float]:
     return lambda other: other / some_arg
 
 
-def test_equality():
+def test_equality() -> None:
     """Ensures that containers can be compared."""
     assert RequiresContext(_same_function) == RequiresContext(_same_function)
 
 
-def test_nonequality():
+def test_nonequality() -> None:
     """Ensures that containers can be compared."""
     assert RequiresContext(_same_function) != RequiresContext(str)
     assert RequiresContext.from_value(1) != RequiresContext.from_value(1)
