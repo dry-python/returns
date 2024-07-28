cefe4c5bb049b24d9fba4058b09f0d9baca9a8db
sobolevn <mail@sobolevn.me>
2024-07-28 17:04:04 +0300

    Fix hypothesis

--- returns/contrib/hypothesis/_entrypoint.py
+++ returns/contrib/hypothesis/_entrypoint.py
@@ -20,12 +20,17 @@ def _setup_hook() -> None:
         RequiresContextIOResult,
         RequiresContextResult,
     )
-    from returns.contrib.hypothesis.containers import strategy_from_container
     from returns.future import Future, FutureResult
     from returns.io import IO, IOResult
     from returns.maybe import Maybe
     from returns.primitives.laws import Lawful
     from returns.result import Result
+
+    def factory(thing):
+        from returns.contrib.hypothesis.containers import (
+            strategy_from_container,
+        )
+        return strategy_from_container(thing)
 
     #: Our types that we register in hypothesis
     #: to be working with ``st.from_type``
@@ -43,7 +48,4 @@ def _setup_hook() -> None:
     )
 
     for type_ in registered_types:
-        st.register_type_strategy(
-            type_,
-            strategy_from_container(type_),
-        )
+        st.register_type_strategy(type_, factory)
