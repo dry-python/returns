410e0c1ba97fb6dcf3c8e540fb98b9608b016844
sobolevn <mail@sobolevn.me>
2024-07-28 17:20:05 +0300

    Fix container resolution

--- returns/contrib/hypothesis/_entrypoint.py
+++ returns/contrib/hypothesis/_entrypoint.py
@@ -8,10 +8,17 @@ interfaces won't be registered!
 
 """
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Callable, Sequence, Type, TypeVar
+
+if TYPE_CHECKING:
+    from returns.primitives.laws import Lawful
+
+_Inst = TypeVar('_Inst', bound='Lawful')
+
 
 def _setup_hook() -> None:
-    from typing import Sequence, Type
-
     from hypothesis import strategies as st
 
     from returns.context import (
@@ -23,14 +30,19 @@ def _setup_hook() -> None:
     from returns.future import Future, FutureResult
     from returns.io import IO, IOResult
     from returns.maybe import Maybe
-    from returns.primitives.laws import Lawful
     from returns.result import Result
 
-    def factory(thing):
-        from returns.contrib.hypothesis.containers import (
-            strategy_from_container,
-        )
-        return strategy_from_container(thing)
+    def factory(
+        container_type: Type[_Inst],
+    ) -> Callable[[Any], st.SearchStrategy[_Inst]]:
+        def decorator(thing: Any) -> st.SearchStrategy[_Inst]:
+            from returns.contrib.hypothesis.containers import (
+                strategy_from_container,
+            )
+            return strategy_from_container(
+                container_type,
+            )(thing)
+        return decorator
 
     #: Our types that we register in hypothesis
     #: to be working with ``st.from_type``
@@ -48,4 +60,4 @@ def _setup_hook() -> None:
     )
 
     for type_ in registered_types:
-        st.register_type_strategy(type_, factory)
+        st.register_type_strategy(type_, factory(type_))
