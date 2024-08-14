8f057761bf6eef2bae33e11f2b3b61475f6690f9
RomanMIzulin <dazyra@gmail.com>
2024-08-14 19:17:42 +0400

    Update tests/test_result/test_result_methods.py
    Co-authored-by: sobolevn <mail@sobolevn.me>

--- tests/test_result/test_result_methods.py
+++ tests/test_result/test_result_methods.py
@@ -15,10 +15,11 @@ from returns.result import Failure, Success
     (
         (
             IOResult.from_value(1),
-            IOResult.from_value(2),
-            IOResult.from_failure(None),
+            IOResult.from_failure(2),
+            IOResult.from_value(3),
+            IOResult.from_failure(4),
         ),
-        ([IO(1), IO(2)], [IO(None)]),
+        ([IO(1), IO(2)], [IO(3), IO(4)]),
     ),
     (
         (Some(1), Some(2), Nothing),
