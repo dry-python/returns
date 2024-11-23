e09e762e0560473af1c35e5b50d1d6e3c3f8c6d0
Roman Matveev <roman.matveev@mysky.com>
2024-11-23 19:44:11 +0400

    wip

---
+++ returns/methods/gather.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+from typing import Awaitable, Iterable
+
+import anyio
+from returns.future import Future, FutureResult
+from returns.io import IOResult
+
+
+
+async def gather(
+    containers: Iterable[
+        Awaitable,
+    ],
+) -> tuple[IOResult, ...]:
+    """
+    Execute multiple coroutines concurrently and return their wrapped results.
+
+    .. code:: python
+
+      >>> import anyio
+      >>> from returns.methods.gather import gather
+      >>> from returns.io import IOSuccess
+
+      >>> async def coro():
+      ...    return 1
+      >>> assert anyio.run(gather([coro()])) == (IOSuccess(1),)
+      >>> container = FutureResult(coro())
+      >>> assert anyio.run(gather([container.awaitable])) == (IOSuccess(1),)
+
+    """
+
+    async with anyio.create_task_group() as tg:
+        containers_t = tuple(containers)
+        results: list[IOResult | None] = len(containers_t)*[IOResult(None)]
+
+        async def run_task(coro: Awaitable, index: int):
+            results[index] = await FutureResult(coro)
+
+        for i, coro in enumerate(containers_t):
+            if i >= len(results):
+                results = results + 2*len(results)*[None]
+            tg.start_soon(run_task, coro, i)
+    return tuple(results)
+
