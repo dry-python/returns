8317f599ba694ab2441334e51f331cda88e7f3b5
Pradeep Kumar <gohanpra@gmail.com>
2025-03-11 02:32:56 +0530

    Accept user-defined strategies (#2061)

--- tests/test_contrib/test_hypothesis/test_type_resolution.py
+++ tests/test_contrib/test_hypothesis/test_type_resolution.py
@@ -1,5 +1,5 @@
 from collections.abc import Sequence
-from typing import Any
+from typing import Any, Final
 
 import pytest
 from hypothesis import given
@@ -15,12 +15,25 @@ from returns.context import (
     RequiresContextResult,
     RequiresContextResultE,
 )
+from returns.contrib.hypothesis.laws import (
+    _Settings,  # noqa: PLC2701
+    interface_strategies,
+    lawful_interfaces,
+    register_container,
+)
+from returns.contrib.hypothesis.type_resolver import (
+    StrategyFactory,
+    apply_strategy,
+    look_up_strategy,
+    strategies_for_types,
+)
 from returns.future import Future, FutureResult
 from returns.io import IO, IOResult, IOResultE
 from returns.maybe import Maybe
 from returns.pipeline import is_successful
 from returns.primitives.laws import Lawful
-from returns.result import Result, ResultE
+from returns.result import Result, ResultE, Success
+from test_hypothesis.test_laws import test_custom_type_applicative
 
 _all_containers: Sequence[type[Lawful]] = (
     Maybe,
@@ -100,3 +113,143 @@ def test_custom_readerresult_types_resolve(
         assert isinstance(real_result.unwrap(), int)
     else:
         assert isinstance(real_result.failure(), str)
+
+
+DEFAULT_RESULT_STRATEGY: Final = (
+    "one_of(builds(from_value, shared(sampled_from([<class 'NoneType'>, "
+    "<class 'bool'>, <class 'int'>, <class 'float'>, <class 'str'>, "
+    "<class 'bytes'>]), key='typevar=~_FirstType').flatmap(from_type)), "
+    "builds(from_failure, shared(sampled_from([<class 'NoneType'>, "
+    "<class 'bool'>, <class 'int'>, <class 'float'>, <class 'str'>, "
+    "<class 'bytes'>]), "
+    "key='typevar=~_SecondType').flatmap(from_type)))"
+)
+
+
+def test_register_container_with_no_strategy() -> None:
+    """Check that a container without a strategy gets a strategy."""
+    container_type = Result
+
+    with register_container(
+        container_type,
+        settings=_Settings(
+            settings_kwargs={}, use_init=False, container_strategy=None
+        ),
+    ):
+        strategy_factory = look_up_strategy(container_type)
+
+    assert (
+        _strategy_string(strategy_factory, container_type)
+        == DEFAULT_RESULT_STRATEGY
+    )
+
+
+def test_register_container_with_strategy() -> None:
+    """Check that when a container has an existing strategy, we drop it."""
+    container_type = Result
+
+    with (
+        strategies_for_types({
+            container_type: st.builds(container_type, st.integers())
+        }),
+        register_container(
+            container_type,
+            settings=_Settings(
+                settings_kwargs={}, use_init=False, container_strategy=None
+            ),
+        ),
+    ):
+        strategy_factory = look_up_strategy(container_type)
+
+    assert (
+        _strategy_string(strategy_factory, container_type)
+        == DEFAULT_RESULT_STRATEGY
+    )
+
+
+def test_register_container_with_setting() -> None:
+    """Check that we prefer a strategy given in settings."""
+    container_type = Result
+
+    with register_container(
+        container_type,
+        settings=_Settings(
+            settings_kwargs={},
+            use_init=False,
+            container_strategy=st.builds(Success, st.integers()),
+        ),
+    ):
+        strategy_factory = look_up_strategy(container_type)
+
+    assert (
+        _strategy_string(strategy_factory, container_type)
+        == 'builds(Success, integers())'
+    )
+
+
+def test_interface_strategies() -> None:
+    """Check that ancestor interfaces get resolved to the concrete container."""
+    container_type = test_custom_type_applicative._Wrapper  # noqa: SLF001
+
+    with interface_strategies(
+        container_type,
+        settings=_Settings(
+            settings_kwargs={}, use_init=False, container_strategy=None
+        ),
+    ):
+        strategy_factories_inside = _interface_factories(container_type)
+
+    assert _strategy_strings(strategy_factories_inside, container_type) == [
+        "builds(from_value, shared(sampled_from([<class 'NoneType'>,"
+        " <class 'bool'>, <class 'int'>, <class 'float'>, <class 'str'>,"
+        " <class 'bytes'>]), key='typevar=~_FirstType').flatmap(from_type))",
+        "builds(from_value, shared(sampled_from([<class 'NoneType'>,"
+        " <class 'bool'>, <class 'int'>, <class 'float'>, <class 'str'>,"
+        " <class 'bytes'>]), key='typevar=~_FirstType').flatmap(from_type))",
+    ]
+
+
+def test_interface_strategies_with_settings() -> None:
+    """Check that we prefer the strategy in the settings."""
+    container_type = test_custom_type_applicative._Wrapper  # noqa: SLF001
+
+    with interface_strategies(
+        container_type,
+        settings=_Settings(
+            settings_kwargs={},
+            use_init=False,
+            container_strategy=st.builds(container_type, st.integers()),
+        ),
+    ):
+        strategy_factories_inside = _interface_factories(container_type)
+
+    assert _strategy_strings(strategy_factories_inside, container_type) == [
+        'builds(_Wrapper, integers())',
+        'builds(_Wrapper, integers())',
+    ]
+
+
+def _interface_factories(type_: type[Lawful]) -> list[StrategyFactory | None]:
+    return [
+        look_up_strategy(interface) for interface in lawful_interfaces(type_)
+    ]
+
+
+def _strategy_strings(
+    strategy_factories: Sequence[StrategyFactory | None], type_: type[object]
+) -> list[str]:
+    return [
+        _strategy_string(strategy_factory, type_)
+        for strategy_factory in strategy_factories
+    ]
+
+
+def _strategy_string(
+    strategy_factory: StrategyFactory | None, type_: type[object]
+) -> str:
+    """Return an easily testable string representation."""
+    return (
+        str(None)
+        if strategy_factory is None
+        else str(apply_strategy(strategy_factory, type_))
+    )
